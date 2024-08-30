3cfc07de88cf0f927027db34c3cbbb128425bc93
aanchalgt <aanchal.gt@gmail.com>
2024-08-30 10:34:02 -0400

    Iteration 4 - add stats

--- utils_gupta.py
+++ utils_gupta.py
@@ -1,15 +1,23 @@
-''' ITERATION 3
+''' ITERATION 4
 
 Module: Datapulse Analytics - Insight at your fingertips.
 
-In this iteration, we will declare some more variables.
-We are going to convert our custom byline to a multiline f-string to make it easy to display the new information.'''
+In this iteration, we will import the statistics module and try some basic statistics using Python.
+The statistics module offers methods to calculate mean and standard deviation.
+- min() is a built-in function to find the smallest value passed in
+- max() is a built-in function to find the largest value passed in '''
 
 
+
+####################################
+#Importing statistics module at the top
+####################################
+
+import statistics
 
 #####################################
-#Declare global variables - we will display this information at the end in a smarter byline 
-####################################
+#Declare global variables 
+#####################################
 
 #boolean variable to indicate the field of company is a Global CRO
 company_field : bool = True
@@ -19,6 +27,26 @@ years_in_operation: int = 16
 services_offered: list = ["Data Analysis", "Machine Learning", "Clinical Research"]
 #List of floats representing individual client satisfaction scores 
 client_satisfaction_scores: list = [4.8, 4.9, 4.7, 5.0, 4.9]
+#list of number of projects in the last 5 years
+num_of_projects: list = [23, 19, 21, 19, 26]
+
+
+#####################################
+# Calculating basic statistics, before we declare the byline.
+# We do this to have calculated values ready for use in the byline.
+#####################################
+
+#calculating basic stats using built-in functions min(),max() and statistics module functions mean() and stdev().
+min_score: float = min(client_satisfaction_scores)
+max_score: float = max(client_satisfaction_scores)
+mean_score: float = statistics.mean(client_satisfaction_scores)
+stdev_score: float = statistics.stdev(client_satisfaction_scores)
+
+min_num: float = min(num_of_projects)
+max_num: float = max(num_of_projects)
+mean_num: float = statistics.mean(num_of_projects)
+stdev_num: float = statistics.stdev(num_of_projects)
+
 
 
 #####################################
@@ -34,6 +62,19 @@ Is a Global CRO:    {company_field}
 Years in operation: {years_in_operation}
 Services offered:   {services_offered}
 Client rating:      {client_satisfaction_scores}
+No. of Projects:    {num_of_projects}
+
+Minimum of Client Satisfaction score: {min_score}
+Maximum of Client Satisfaction score: {max_score}
+Mean of Client Satisfaction score: {mean_score}
+Standard Deviation of Client Satisfaction score: {stdev_score}
+
+Minimum of Number of projects: {min_num}
+Maximum of Number of projects: {max_num}
+Mean of Number of projects: {mean_num}
+Standard Deviation of Number of projects: {stdev_num}
+
+
 """
 
 #####################################
